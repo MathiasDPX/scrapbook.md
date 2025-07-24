91ca0d4d947964416c34e9ac1d1df5f10dd44c75
MathiasDPX <mathias@dupeux.net>
2025-07-24 16:39:25 +0200

    add image generator

---
+++ image.py
@@ -0,0 +1,60 @@
+from PIL import Image, ImageDraw, ImageFont
+
+def get_empty():
+    return Image.new("RGB", (0,0))
+
+def draw_card(text:str, author:str):
+    img = Image.open("base.png").convert("RGB")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("font.ttf", 48)
+    attrib_font = ImageFont.truetype("font.ttf", 24)
+    
+    max_width = img.width - 40
+    words = text.split()
+    lines = []
+    current_line = ""
+
+    for word in words:
+        test_line = current_line + " " + word if current_line else word
+        w = draw.textlength(test_line, font=font)
+        if w <= max_width:
+            current_line = test_line
+        else:
+            lines.append(current_line)
+            current_line = word
+        if len(lines) == 4:
+            break
+
+    if len(lines) < 4 and current_line:
+        lines.append(current_line)
+
+    if len(lines) == 4 and (len(words) > sum(len(line.split()) for line in lines)):
+        while True:
+            if not lines[-1]:
+                break
+            test_line = lines[-1] + "..."
+            w = draw.textlength(test_line, font=font)
+            if w <= max_width:
+                lines[-1] = lines[-1] + "..."
+                break
+            else:
+                # Remove last word
+                lines[-1] = " ".join(lines[-1].split()[:-1])
+
+    # Draw lines
+    y = 20
+    for line in lines:
+        draw.text((24, y), line, font=font, fill=(255,)*4)
+        y += 48 + 8
+
+    author_w = draw.textlength(author, font=attrib_font)
+    draw.text((img.width-author_w-20, img.height-60), author, font=attrib_font)
+
+    return img
+
+if __name__ == "__main__":
+    img = draw_card(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales, lectus a gravida posuere, erat justo vestibulum nisl, non venenatis orci turpis ac nulla. Cras dictum et felis feugiat rutrum. Integer a odio tempus, luctus nisl ut, ultricies lacus. Nunc in nibh felis. Nulla mollis tincidunt mauris. Pellentesque pellentesque auctor laoreet. Maecenas finibus accumsan lobortis. Praesent vel ornare magna. In suscipit pharetra ipsum pellentesque sagittis.",
+        "@author"
+    )
+    img.show()
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from flask import Flask, redirect, abort
+from flask import Flask, redirect, abort, send_file
+from io import BytesIO
 from bridge import *
+from image import *
 
 app = Flask(__name__)
+
+def serve_image(img):
+    img_io = BytesIO()
+    img.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
 
 
 @app.route("/", methods=["GET"])
 def index():
     return redirect("https://github.com/MathiasDPX/scrapbook.md")
-
-
-@app.route("/post/<postid>", methods=["GET"])
-def get_post(postid:str):
-    return abort(501)
 
 
 @app.route("/latest-post/<username>", methods=["GET"])
@@ -20,7 +23,15 @@ def get_latest_post(username:str):
     if type(user) == int:
         return abort(user)
 
-    return abort(501)
+    if len(user.posts) == 0:
+        img = get_empty()
+    else:
+        img = draw_card(
+            text=user.posts[0].text,
+            author="@"+username
+        )
+    
+    return serve_image(img)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
